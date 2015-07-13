2430c2676111e487a21b5f550e4569190a04e60c
Kevin O'Connor <koconnor@ixl.com>
2015-07-13 12:20:07 -0700

    Made some changes to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,24 +3,41 @@ var react = require('gulp-react');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 var reactify = require('reactify');
+var watchify = require('watchify');
 
-function transpileJS(path) {
-  var file = path.split('/').pop();
-  console.log(file);
-  var bundler = browserify(path);
+function transpileJS() {
+  // create browserify bundler for converting commonjs into browser-ready js
+  var b = browserify({
+    cache: {},
+    packageCache: {}
+  });
 
-  var stream = bundler
-    .transform(reactify)
-    .bundle();
+  // Wrap the bundler with watchify. This will cause the js to be re-bundled any time
+  // any individual js file is modified
+  b = watchify(b);
 
-  stream.on('error', function (err) { console.error(err.toString()) });
-  return stream
-    .pipe(source(file))
-    .pipe(gulp.dest('build'));
+  b.on('update', function() { bundle(b); });
+
+  // MapMaker.jsx is the 'main' js file to be executed
+  b.add('./MapMaker.jsx');
+
+  bundle(b);
+}
+
+function bundle(b) {
+  b
+   .transform('reactify')
+   .bundle()
+   .pipe(source('./MapMaker.jsx'))
+   .pipe(gulp.dest('./build'));
 }
 
 gulp.task('watch', function() {
   gulp.watch('./*.jsx', function(event) {
     transpileJS(event.path);
   });
+});
+
+gulp.task('default', function() {
+  transpileJS();
 });
