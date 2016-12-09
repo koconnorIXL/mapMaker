e7faab75c3ab96b832a60c9e556ca1a3da9137a6
koconnor29 <kevinoconnor29@gmail.com>
2016-12-09 12:44:15 -0800

    Adding some scripts that were used a while ago for generating continent datasets... not anymore

---
+++ CreateTopojsonData_continent_country_groups.js
@@ -0,0 +1,80 @@
+var fs = require('fs');
+var d3 = require('d3');
+var topojson = require('topojson');
+var MapColoring = require('./MapColoring.js');
+var removeSmallIslands = require('./RemoveSmallIslands.js').removeSmallIslands;
+var ResolveBorders = require('./FixDisputedBoundaries.js');
+
+var datasetsToModify = [
+  'countries_custom_disputed_boundaries.json'
+];
+
+var CONTINENTS = [
+  'North America',
+  'South America',
+  'Africa',
+  'Europe',
+  'Asia',
+  'Oceania',
+  'Antarctica',
+  'Seven seas (open ocean)'
+];
+
+function getFileName(continentName) {
+  continentName = continentName
+    .replace(/ /g, '_')
+    .replace('(', '')
+    .replace(')', '')
+  return 'topojsonDatasets/' + continentName + '_countries.json'
+}
+
+var SIMPLIFY_MINIMUM_AREA_GLOBAL = 0.02;
+var SIMPLIFY_MINIMUM_AREA_LOCAL = 0.001;
+
+var SMALL_ISLAND_THRESHOLD_GLOBAL = 3;
+var SMALL_ISLAND_THRESHOLD_LOCAL = 0.0001;
+
+function getFreshData(filename) {
+  // Get the geojson data.
+  var data = JSON.parse(fs.readFileSync('geojsonDatasets/' + filename));
+
+  return data.features.map(function(feature) {
+    return {
+      'type': 'FeatureCollection',
+      'features': [feature]
+    };
+  });
+}
+  
+
+datasetsToModify.forEach(function(filename) {
+
+  // Get the geojson data.
+  var data = JSON.parse(fs.readFileSync('geojsonDatasets/' + filename));
+
+  var standardProjection = d3.geo.equirectangular();
+  var simplifyOptions = {
+    'coordinate-system': 'cartesian',
+    'minimum-area': SIMPLIFY_MINIMUM_AREA_LOCAL
+  };
+  var options = {
+    "property-transform": function(x) { return x.properties; }
+  };
+
+
+  CONTINENTS.forEach(function(continentName) {
+    var continentData = data.features.filter(function(feature) { 
+      return feature.properties.continent === continentName;
+    }); 
+    var featureCollection = { 
+      type: 'FeatureCollection',
+      features: continentData
+    };
+    
+    var topology = topojson.topology({collection: featureCollection}, options);
+    topology.objects[continentName] = topology.objects.collection;
+    delete topology.objects.collection;
+    fs.writeFile(getFileName(continentName), JSON.stringify(topology));
+  });
+});
+
